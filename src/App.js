e9a91e6698cf5a5671221d62e015493469bdb2c3
Vishal Prajapati <52890952+Vishal01-git@users.noreply.github.com>
2021-06-21 13:35:24 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useRef } from "react";
+//Adding components
+import Player from "./components/Player";
+import Song from "./components/Song";
+import Library from "./components/Library";
+import Nav from "./components/Nav";
+import "./style/app.scss";
+import data from "./data";
 function App() {
+  //References
+  const audioRef = useRef(null);
+  //State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  //state
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0,
+    animationPercentage: 0,
+  });
+  const [libraryStatus, setLibraryStatus] = useState(false);
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    //Calculate Percentage
+    const roundedCurrent = Math.round(current);
+    const rounedDuration = Math.round(duration);
+    const animation = Math.round((roundedCurrent / rounedDuration) * 100);
+    setSongInfo({
+      ...songInfo,
+      currentTime: current,
+      duration,
+      animationPercentage: animation,
+    });
+  };
+
+  const songEndHandler = async () => {
+    let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
+
+    await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+    if (isPlaying) audioRef.current.play();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${libraryStatus ? "library-active" : ""}`}>
+      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
+      <Song currentSong={currentSong} />
+      <Player
+        setSongInfo={setSongInfo}
+        setSongs={setSongs}
+        songInfo={songInfo}
+        audioRef={audioRef}
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+        songs={songs}
+        setCurrentSong={setCurrentSong}
+      />
+      <Library
+        setSongs={setSongs}
+        audioRef={audioRef}
+        songs={songs}
+        setCurrentSong={setCurrentSong}
+        isPlaying={isPlaying}
+        libraryStatus={libraryStatus}
+      />
+      <audio
+        onTimeUpdate={timeUpdateHandler}
+        onLoadedMetadata={timeUpdateHandler}
+        ref={audioRef}
+        src={currentSong.audio}
+        onEnded={songEndHandler}
+      ></audio>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,69 @@
+import { v4 as uuidv4 } from "uuid";
+
+function chillHop() {
+  return [
+    {
+      name: "Take Your Time",
+      artist: "Misha, Jussi Halme, cocabona",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/05/2473c60e471fe9b40e246bf7711c87d3d6ea69a7-1024x1024.jpg",
+      id: uuidv4(),
+      active: true,
+      color: ["#E77170", "#2E385B"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=15076",
+    },
+
+    {
+      name: "travelbag",
+      artist: "mommy, Sleepy Fish",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/05/1245c0271290a3196328c0cf4aaa910cd873dfe7-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#5F96D7", "#EBD7D2"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=19058",
+    },
+    {
+      name: "Take Your Time",
+      artist: "Misha, Jussi Halme, cocabona",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/05/2473c60e471fe9b40e246bf7711c87d3d6ea69a7-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#E77170", "#2E385B"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=15076",
+    },
+    {
+      name: "travelbag",
+      artist: "mommy, Sleepy Fish",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/05/1245c0271290a3196328c0cf4aaa910cd873dfe7-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#5F96D7", "#EBD7D2"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=19058",
+    },
+    {
+      name: "Take Your Time",
+      artist: "Misha, Jussi Halme, cocabona",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/05/2473c60e471fe9b40e246bf7711c87d3d6ea69a7-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#E77170", "#2E385B"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=15076",
+    },
+    {
+      name: "travelbag",
+      artist: "mommy, Sleepy Fish",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/05/1245c0271290a3196328c0cf4aaa910cd873dfe7-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#5F96D7", "#EBD7D2"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=19058",
+    },
+  ];
+}
+
+export default chillHop;
